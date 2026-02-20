6d852c5de51d5981b9ba0ccb5552a6944e1c5d03
kjayesh <kjayesh@ncsu.edu>
2026-02-19 23:43:21 -0500

    Initial spring boot project setup

---
+++ src/main/java/com/booked/booked/HomeController.java
@@ -0,0 +1,17 @@
+package com.booked.booked;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+
+    }
+    
+}
---
+++ src/test/java/com/booked/booked/BookedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booked.booked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
